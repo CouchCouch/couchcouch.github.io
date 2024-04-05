357668580782b535d908622e06e97ed1a9e12f0d
Ryan Couchman <rcouchman_9@roadrunner.com>
2024-04-04 20:36:08 -0400

    updated css, added link to my github profile typewrite animation no longer works

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 var i = 0;
-function typeWriter(word, id, speed) {
-  i = 0;
-  typeWriterHelper(word, id, speed);
-}
+var speed = 100 // Time to passe in ms for typewriter animation
 
-function typeWriterHelper(word, id, speed) {
+/**
+ * Helper function for typeWriter
+ * @param {string} word 
+ * @param {string} id 
+ */
+function typeWriterHelper(word, id) {
   if (i < word.length) {
-    document.getElementById(id).innerHTML += word.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+    document.getElementById(id).innerHTML += word.charAt(this.i);
+    this.i++;
+    setTimeout(typeWriterHelper(word, id), 100);
   }
 }
+
+/**
+ * Function to animate text in a typewriter fashion
+ * @param {string} word 
+ * @param {string} id 
+ */
+function typeWriter(word, id) {
+  this.i = 0;
+  typeWriterHelper(word, id);
+}
+
