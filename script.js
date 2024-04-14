dfdc9f465337e7989d1ab30e963b9888f3f08574
Ryan Couchman <rcouchman_9@roadrunner.com>
2024-04-14 00:03:53 -0400

    added dark mode and learned how to use cookies

--- script.js
+++ script.js
@@ -28,6 +28,51 @@ function typeWriter(word, id) {
  * Function to toggle dark mode
  */
 function toggleDarkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+  if (getCookie("mode") === "dark") {
+    document.body.classList.remove("dark-mode");
+    document.getElementById("light_mode_buttons").style.display = "block";
+    document.getElementById("dark_mode_buttons").style.display = "none";
+    setCookie("mode", "", -1); // remove the cookie
+  } else {
+    document.body.classList.add("dark-mode");
+    document.getElementById("light_mode_buttons").style.display = "none";
+    document.getElementById("dark_mode_buttons").style.display = "block";
+    setCookie("mode", "dark", 365); // set the cookie to expire in 365 days
+  }
+}
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Function to get a cookie
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+window.onload = function() {
+  if (getCookie("mode") === "dark") {
+    document.body.classList.add("dark-mode");
+    document.getElementById("light_mode_buttons").style.display = "none";
+    document.getElementById("dark_mode_buttons").style.display = "block";
+    setCookie("mode", "dark", 365); // set the cookie to expire in 365 days
+  } else {
+    document.getElementById("light_mode_buttons").style.display = "block";
+    document.getElementById("dark_mode_buttons").style.display = "none";
+    setCookie("mode", "", -1); // remove the cookie
+  }
 }
