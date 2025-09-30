e5e1ae3abeda55e7aece01aeb4ed166b2cd5e281
Ryan Couchman <rfc3567@rit.edu>
2025-09-30 13:11:58 -0400

    Resume and home view updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,11 @@
     <div>
         <h3>Hello, my name is Ryan Couchman</h3>
         <p>Welcome to my personal site. I will use this as a place to feature my projects and share what I am working on with the world</p>
+        <p>I am currently a 3<sup>rd</sup> year software engineering major at RIT seeking internships for spring and summer of 2026</p>
         <div class="mt-4">
             <h3>Current Projects:</h3>
+            <h4>School</h4>
+            <p>Currently enrolled in Software System Requirements and Architecture (SWEN-440), Engineering Secure Software (SWEN-331), Aalysis of Algoritms (CSCI-261), Intro to Bioinformatics (BIOL-130), Maps, Mapping and Geospatial Technology (IGME-382), Introduction to Geographic Information Systems (IGME-384)</p>
             <h4>Inventory Management System</h4>
             <p>Creating a basic inventory management system that will handle basic CRUD operations and a checkout system for items using PostgreSQL, Go, React with Vite and Tailwind CSS</p>
             <p>Work in Progress</p>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -5,24 +5,43 @@
             <dd>email: <a href="mailto:rfc3567@g.rit.edu">rfc3567@g.rit.edu</a></dd>
             <dd>LinkedIn: <a href="https://www.linkedin.com/in/ryan-couchman"> www.linkedin.com/in/ryan-couchman</a></dd>
         <dt>Objective</dt>
-            <dd>To find a co-op as a Software Engineer for May or August to December 2025</dd>
+            <dd>To find a co-op as a Software Engineer for May or January to August 2026</dd>
         <dt>Education</dt>
             <dl>
                 <div class="sublist">
                     <dt>Rochester Institute of Technology</dt>
                     <dd>Bachelor of Science in Software Engineering Expected Graduation: December 2027</dd>
-                    <dd>GPA 3.5, Dean's List Fall 2023, Dean's List Spring 2024</dd>
-                    <dd>Related Courses: Programming Language Concepts, Data Structures & Problem Solving, Engineering of Subsystems, Software Engineering, Software Process and Project Management, Web Engineering</dd>
+                    <dd>GPA 3.4, Dean's List Fall 2023, Dean's List Spring 2024</dd>
+                    <dd>Related Courses: Data Structures & Problem Solving, Engineering of Subsystems, Software Process and Project Management, Web Engineering, Software Design for Computing Systems, Human Centered Requirements and Design, Engineering Secure Software, Software Architecture and Requirements, Analysis of Algorithms</dd>
                 </div>
             </dl>
         <dt>Skills</dt>
-            <dd>Languages: Python, C, C++, Java, SpringBoot, AngularJS, PostgreSQL, React, UML, Go, TailwindCSS</dd>
-            <dd>OS and Software: Linux, Windows, MS Office, Google Workspace, nginx, Git</dd>
+            <dd>Languages: Python, C, C++, Java, SQL, React, UML, Go, Linux Shell Script</dd>
+            <dd>Frameworks: Spring Boot, Flask, React, Tailwind CSS, gRPC</dd>
+            <dd>OS and Software: Linux, Windows, MS Office, Google Workspace, nginx, Git, ArcGIS, Make</dd>
             <dd>Hardware: Fused Filament Fabrication 3D Printing, Arduino, Vex Robotics</dd>
+        <dt>Experience</dt>
+        <dl>
+            <div id="sublist" class="sublist">
+                <dt>Intern at Slide</dt>
+                <dd>June 2025 - August 2025</dd>
+                <dd>Worked on their BCDR product for MSPs</dd>
+                <dd>Created a cross backup per agent file indexing and searching with another intern</dd>
+                <dd>Worked in an agile environment at a startup</dd>
+                <dt>Course Assistant Rochester Institute of Technology</dt>
+                <dd>Software Engineering 101, August 2024 - December 2024</dd>
+                <dd>Web Engineering, January 2025 - May 2025</dd>
+                <dd>Grade homework and assist students with assignments</dd>
+                <dt>YMCA Buffalo Niagara</dt>
+                <dd>Rock Wall Specialist, July 2023 - August 2023, June 2024 - August 2024</dd>
+                <dd>Operated a rock wall and zip-line at an outdoor day camp for children and teens aged 5-15</dd>
+            </div>
+        </dl>
         <dt>Projects</dt>
             <dl>
                 <div class="sublist">
                     <dt>nutRITion</dt>
+                        <dd>BrickHack 2025</dd>
                         <dd>Created and deployed a web app that used AI to create a nutritious meal from items available at RIT dining locations</dd>
                         <dd>Worked on the initial design for the frontend and then did some debugging and optimization on other parts of the project</dd>
                     <dt>Embedded MIDI Player</dt>
@@ -46,18 +65,6 @@
                         <dd>Intro to Software Engineering January 2024 - May 2024</dd>
                         <dd>Created a charity focused web app using AngularJS and Java with SpringBoot</dd>
                         <dd>Collaborated in a team of four using Agile and Scrum methodologies</dd>
-                </div>
-            </dl>
-        <dt>Experience</dt>
-            <dl>
-                <div id="sublist" class="sublist">
-                    <dt>Course Assistant Rochester Institute of Technology</dt>
-                        <dd>Software Engineering 101, August 2024 - December 2024</dd>
-                        <dd>Web Engineering, January 2025 - May 2025</dd>
-                        <dd>Grade homework and assist students with assignments</dd>
-                    <dt>YMCA Buffalo Niagara</dt>
-                        <dd>Rock Wall Specialist, July 2023 - August 2023, June 2024 - August 2024</dd>
-                        <dd>Operated a rock wall and zip-line at an outdoor day camp for children and teens aged 5-15</dd>
                 </div>
             </dl>
         <dt>Awards</dt>
