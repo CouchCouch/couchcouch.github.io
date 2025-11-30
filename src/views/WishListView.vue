a44dc74e9a9949fd83840673c5c31ecce5deedae
Ryan Couchman <rfc3567@rit.edu>
2025-11-29 23:34:00 -0500

    Added wishlist, personal site for personal use

---
+++ src/views/WishListView.vue
@@ -0,0 +1,38 @@
+<template>
+    <h3>Ryan's 2025 Christmas Wish List</h3>
+    <div class="wishlist">
+        <ul>
+            <li>Hiking Pants Kuhl Deceptr or Renegade (Mom has pictures) Something that is a dark khaki color</li>
+            <li>Beanie<ul>
+                <li><a href="https://www.backcountry.com/smartwool-cozy-cabin-hat">Backcountry</a>($30 At the time of writing)</li>
+                <li><a href="https://www.rei.com/product/194148/fjallraven-tab-hat">REI</a> Different bvut still good, ($28 at time of writing)</li>
+            </ul>
+            </li>
+            <li>Toaks Titanium 750 ml pot <a href="https://www.rei.com/product/139459/toaks-titanium-750-ml-pot">REI</a></li>
+            <li>insulated camping blanket<ul>
+                <li><a href="https://www.rei.com/search?q=blanket">REI search</a> do what looks right, I trust you</li>
+                <li><a href="https://www.rumpl.com/">Rumpl</a></li>
+                <li>I can also send you a link to get some cheaper until 12/15 $10 shipping unfortunately</li>
+                <li>I can also get free shipping at REI or pickup is free</li>
+            </ul>
+            </li>
+            <li>Portable Jump Starter, I havent had any issue but might not be the worst thing <a href="https://www.popularmechanics.com/cars/g38293716/best-portable-jump-starters/">Popular Mechanics Ranking</a></li>
+            <li>Shirts, Red, Maroon, Maybe a Dark Green-Grey, grey. blue, muted blue</li>
+            <li>Fleece</li>
+            <li>Hoodie, Maybe a grey-green or a muted blue<ul>
+                <li><a href="https://www.rei.com/product/210896/mountain-hardwear-summit-grid-hoody-mens">Mountain Hardwear Summit Grid</a></li>
+            </ul>
+            </li>
+            <li>Down Puffy, the ones I really want are 200 minimum<ul>
+                <li><a href="https://www.rei.com/product/250864/cotopaxi-fuego-hooded-down-jacket-mens">Cotopaxi Fuego</a></li>
+                <li><a href="https://www.rei.com/product/176548/fjallraven-expedition-pack-down-hoodie-mens">Fjalraven Expedition</a></li>
+                <li><a href="https://www.rei.com/product/249150/patagonia-nano-puff-insulated-hoody-mens">Patagonia Nano Puff</a></li>
+                <li><a href="https://www.rei.com/product/208593/patagonia-down-sweater-hoody-mens">Patagonia Down Sweater</a></li>
+            </ul>
+            </li>
+            <li>Checks</li>
+            <li>Cash</li>
+            <li>Wegmans Giftcard</li>
+        </ul>
+    </div>
+</template>
