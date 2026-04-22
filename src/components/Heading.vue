4a1b1543e9145e9f66fdbcdf0e9e984556916004
Ryan Couchman <rfc3567@rit.edu>
2026-04-22 16:39:25 -0400

    Added a gitea link

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,5 +1,5 @@
 <script setup>
-import  { Github, Linkedin, FileUser } from "lucide-vue-next"
+import  { Github, Linkedin, FileUser, GitMerge } from "lucide-vue-next"
 </script>
 <template>
     <header class="flex my-2">
@@ -7,20 +7,7 @@ import  { Github, Linkedin, FileUser } from "lucide-vue-next"
             <h3 class="text-2xl md:text-3xl p-0 m-0 font-bold">Ryan Couchman</h3>
         </RouterLink>
         <div class="ml-auto mr-0">
-            <div class="inline-flex dark:hidden">
-                <RouterLink to="/resume">
-                    <FileUser class="header-icon" />
-                    <label>Resume</label>
-                </RouterLink>
-                <a href="https://www.linkedin.com/in/ryan-couchman">
-                    <Linkedin class="header-icon" />
-                </a>
-                <a href="https://github.com/CouchCouch">
-                    <Github class="header-icon" />
-                </a>
-            </div>
-
-            <div class="hidden dark:inline-flex">
+            <div class="inline-flex">
                 <RouterLink to="/resume">
                     <FileUser class="header-icon" />
                 </RouterLink>
@@ -29,6 +16,9 @@ import  { Github, Linkedin, FileUser } from "lucide-vue-next"
                 </a>
                 <a href="https://github.com/CouchCouch">
                     <Github class="header-icon" />
+                </a>
+                <a href="https://gitea.ryancouchman.me/couch">
+                    <GitMerge class="header-icon" />
                 </a>
             </div>
         </div>
